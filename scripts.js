88e28ca7ccab948a0982695103c4d6bf794e2c87
Mohau Mushi <david1mushi1@gmail.com>
2024-05-15 13:44:48 +0200

    imported data objects and fragments into the index.js

--- scripts.js
+++ scripts.js
@@ -1,51 +1,7 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from "./data.js";
 
-// Created an object literal that holds all of our DOM element references
-const data = {
-  list: {
-    items: document.querySelector("[data-list-items]"),
-    button: document.querySelector("[data-list-button]"),
-    message: document.querySelector("[data-list-message]"),
-    active: document.querySelector("[data-list-active]"),
-    close: document.querySelector("[data-list-close]"),
-    blur: document.querySelector("[data-list-blur]"),
-    image: document.querySelector("[data-list-image]"),
-    title: document.querySelector("[data-list-title]"),
-    subtitle: document.querySelector("[data-list-subtitle]"),
-    description: document.querySelector("[data-list-description]"),
-  },
-  search: {
-    overlay: document.querySelector("[data-search-overlay]"),
-    form: document.querySelector("[data-search-form]"),
-    cancel: document.querySelector("[data-search-cancel]"),
-    title: document.querySelector("[data-search-title]"),
-    genres: document.querySelector("[data-search-genres]"),
-    authors: document.querySelector("[data-search-authors]"),
-  },
-  settings: {
-    overlay: document.querySelector("[data-settings-overlay]"),
-    form: document.querySelector("[data-settings-form]"),
-    cancel: document.querySelector("[data-settings-cancel]"),
-    theme: document.querySelector("[data-settings-theme]"),
-  },
-  header: {
-    search: document.querySelector("[data-header-search]"),
-    settings: document.querySelector("[data-header-settings]"),
-    help: document.querySelector("[data-header-help]"),
-    add: document.querySelector("[data-header-add]"),
-    order: document.querySelector("[data-header-order]"),
-    grid: document.querySelector("[data-header-grid]"),
-    list: document.querySelector("[data-header-list]"),
-    title: document.querySelector("[data-header-title]"),
-    subtitle: document.querySelector("[data-header-subtitle]"),
-  },
-};
+import { data, searchResultFragment, bookListFragment, genreFragment, authorFragment } from './element.js';
 
-//   Created a document fragment, which will be accessible throughout the file
-const searchResultFragment = document.createDocumentFragment();
-const bookListFragment = document.createDocumentFragment();
-const genreFragment = document.createDocumentFragment();
-const authorFragment = document.createDocumentFragment();
 
 let page = 1;
 let matches = books;
